50a54502a393725e3a2a1c3550963c11e99dde4a
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-09-12 20:31:21 +0200

    random select of the questions from datas

--- js/main.js
+++ js/main.js
@@ -8,66 +8,66 @@ createApp({
             datas: [
                 {
                     question: 'Qual è la capitale della Spagna?',
-                    options: {
-                        optionOne: {
+                    options: [
+                        {
                             text: 'Valencia',
                             result: false
                         },
-                        optionTwo: {
+                        {
                             text: 'Barcellona',
                             result: false
                         },
-                        optionThree: {
+                        {
                             text: 'Madrid',
                             result: true
                         },
-                        optionFour: {
+                        {
                             text: 'Ibiza',
                             result: false
                         }
-                    }
+                    ]
                 },
                 {
                     question: 'In che anno Cristoforo Colombo sbarcò in America?',
-                    options: {
-                        optionOne: {
+                    options: [
+                        {
                             text: '1538',
                             result: false
                         },
-                        optionTwo: {
+                        {
                             text: '1492',
                             result: true
                         },
-                        optionThree: {
+                        {
                             text: '1945',
                             result: false
                         },
-                        optionFour: {
+                        {
                             text: '1388',
                             result: false
                         }
-                    }
+                    ]
                 },
                 {
                     question: 'Di che colore è il Golden Bridge di San Francisco?',
-                    options: {
-                        optionOne: {
+                    options: [
+                        {
                             text: 'Rosso',
                             result: true
                         },
-                        optionTwo: {
+                        {
                             text: 'Oro',
                             result: false
                         },
-                        optionThree: {
+                        {
                             text: 'Giallo',
                             result: false
                         },
-                        optionFour: {
+                        {
                             text: 'Blu',
                             result: false
                         }
-                    }
+                    ]
                 }
             ]
         }
@@ -79,9 +79,28 @@ createApp({
 
             playBtnDom.classList.add('d-none');
             gameDom.classList.remove('d-none');
-        },
-        generateUniqueRandomNumber() {
 
+
+            const questionsToDo = [];
+
+            while (questionsToDo.length < this.datas.length) {
+                let singleQuestion = this.datas[this.generateRandomNumber(0, this.datas.length - 1)];
+                if (!questionsToDo.includes(singleQuestion)) {
+                    questionsToDo.push(singleQuestion);
+                }
+            }
+
+            console.log(questionsToDo);
+
+
+        },
+        generateRandomNumber(min, max) {
+            if (min >= max) {
+                throw new Error('Il valore minimo deve essere inferiore al valore massimo.');
+            }
+
+            const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            return randomNumber;
         }
     }
 }).mount('#app')
