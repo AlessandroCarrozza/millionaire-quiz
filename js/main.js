145cdf68b7c807d6b1ebe1b1286efbb11f7c4226
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-09-15 17:31:13 +0200

    split optionClick function

--- js/main.js
+++ js/main.js
@@ -72,6 +72,153 @@ createApp({
                             result: false
                         }
                     ]
+                },
+                {
+                    question: "Qual è l'isola piu grande del mondo?",
+                    options: [
+                        {
+                            text: 'Sardegna',
+                            result: false
+                        },
+                        {
+                            text: 'Australia',
+                            result: false
+                        },
+                        {
+                            text: 'Groenlandia',
+                            result: true
+                        },
+                        {
+                            text: 'Inghilterra',
+                            result: false
+                        }
+                    ]
+                },
+                {
+                    question: "Chi è stato l'ultimo marito di Cleopatra?",
+                    options: [
+                        {
+                            text: 'Giulio Cesare',
+                            result: false
+                        },
+                        {
+                            text: 'Luigi XIV',
+                            result: false
+                        },
+                        {
+                            text: 'Marco Antonio',
+                            result: true
+                        },
+                        {
+                            text: 'El Shaarawy',
+                            result: false
+                        }
+                    ]
+                },
+                {
+                    question: 'Da quale lingua deriva lo spagnolo?',
+                    options: [
+                        {
+                            text: 'Greco',
+                            result: false
+                        },
+                        {
+                            text: 'Latino',
+                            result: true
+                        },
+                        {
+                            text: 'Romano',
+                            result: false
+                        },
+                        {
+                            text: 'Rumeno',
+                            result: false
+                        }
+                    ]
+                },
+                {
+                    question: 'Chi ha vinto la Coppa del Mondo 2014?',
+                    options: [
+                        {
+                            text: 'Olanda',
+                            result: false
+                        },
+                        {
+                            text: 'Argentina',
+                            result: false
+                        },
+                        {
+                            text: 'Spagna',
+                            result: false
+                        },
+                        {
+                            text: 'Germania',
+                            result: true
+                        }
+                    ]
+                },
+                {
+                    question: 'Qual è la galassia piu vicina alla Via Lattea?',
+                    options: [
+                        {
+                            text: 'Andromeda',
+                            result: true
+                        },
+                        {
+                            text: 'Abell',
+                            result: false
+                        },
+                        {
+                            text: 'Boote',
+                            result: false
+                        },
+                        {
+                            text: 'Magellano',
+                            result: false
+                        }
+                    ]
+                },
+                {
+                    question: 'Quanti stati ci sono negli Stati Uniti?',
+                    options: [
+                        {
+                            text: '30',
+                            result: false
+                        },
+                        {
+                            text: '50',
+                            result: true
+                        },
+                        {
+                            text: '40',
+                            result: false
+                        },
+                        {
+                            text: '60',
+                            result: false
+                        }
+                    ]
+                },
+                {
+                    question: 'In che anno è nato Harry Potter?',
+                    options: [
+                        {
+                            text: '1976',
+                            result: false
+                        },
+                        {
+                            text: '1990',
+                            result: false
+                        },
+                        {
+                            text: '1999',
+                            result: false
+                        },
+                        {
+                            text: '1980',
+                            result: true
+                        }
+                    ]
                 }
             ]
         }
@@ -90,48 +237,53 @@ createApp({
         },
         optionClick(index) { // function del click su un opzione
             const optionsDom = document.querySelectorAll('.options .text');
-            const nextBtnDom = document.getElementById('next-btn');
             const pointsDom = document.getElementById('results');
-            const xDom = document.getElementById('x');
-            if (!this.gameOver && this.clickActive) { // se il game è non è over e il click attivo
+            if (!this.gameOver && this.clickActive) { // se il game è non è finito e il click attivo
                 if (this.currentOptions[index].result == false) {
                     // se click su falso
-                    optionsDom[index].classList.add('red')
-                    for (let i = 0; i < this.currentOptions.length; i++) {
-                        if (this.currentOptions[i].result) {
-                            optionsDom[i].classList.add('green')
-                        }
-                    }
-                    this.gameOver = true;
-                    pointsDom.innerHTML = `Hai perso con un punteggio di ${this.userPoints}, ti mancavano ${this.questionsToDo.length - this.userPoints} domanda/e!`;
-                    xDom.classList.remove('d-none');
+                    this.clickFalse(index, optionsDom, pointsDom);
                 } else {
                     // se click su vero
-                    optionsDom[index].classList.add('green')
-                    this.userPoints++;
-                    nextBtnDom.classList.remove('d-none');
-                    this.clickActive = false;
-
-                    const winDom = document.getElementById('win');
-                    const trophyDom = document.getElementById('trophy');
-                    // se il punteggio è uguale alla length delle domande disponibili, lo user vince
-                    if (this.userPoints == this.questionsToDo.length) {
-                        nextBtnDom.classList.add('d-none');
-                        pointsDom.classList.add('d-none');
-                        winDom.classList.remove('d-none');
-                        trophyDom.classList.remove('d-none');
-                    }
+                    this.clickTrue(index, optionsDom, pointsDom);
                 }
+            }
+        },
+        clickFalse(index, optionsDom, pointsDom) {
+            const gameOverDom = document.getElementById('game-over');
+            optionsDom[index].classList.add('red')
+            for (let i = 0; i < this.currentOptions.length; i++) {
+                if (this.currentOptions[i].result) {
+                    optionsDom[i].classList.add('green')
+                }
+            }
+            this.gameOver = true;
+            pointsDom.innerHTML = `Hai perso con un punteggio di ${this.userPoints}, ti mancavano ${this.questionsToDo.length - this.userPoints} domanda/e!`;
+            gameOverDom.classList.remove('d-none');
+        },
+        clickTrue(index, optionsDom, pointsDom) {
+            const nextBtnDom = document.getElementById('next-btn');
+            const winDom = document.getElementById('win');
+            const trophyDom = document.getElementById('trophy');
+            optionsDom[index].classList.add('green')
+            this.userPoints++;
+            nextBtnDom.classList.remove('d-none');
+            this.clickActive = false;
+            // se il punteggio è uguale alla length delle domande disponibili, lo user vince
+            if (this.userPoints == this.questionsToDo.length) {
+                nextBtnDom.classList.add('d-none');
+                pointsDom.classList.add('d-none');
+                winDom.classList.remove('d-none');
+                trophyDom.classList.remove('d-none');
             }
         },
         generateSingleQuestion() {
             // generare la singola domanda con le relative opzioni
             const optionsDom = document.querySelectorAll('.options .text');
+            const nextBtnDom = document.getElementById('next-btn');
             for (let i = 0; i < optionsDom.length; i++) {
                 optionsDom[i].classList.remove('green');
             }
             this.clickActive = true;
-            const nextBtnDom = document.getElementById('next-btn');
             nextBtnDom.classList.add('d-none');
             this.currentOptions = [];
             if (!this.gameOver) {
